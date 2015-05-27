3e89b087ba558e4185153f61bf99dec17361dc58
fallaha13 <fallaha@wit.edu>
2015-05-27 15:44:38 -0700

    Added json data from url, and added mapview delegate to see points on the map

---
+++ GetOnThatBus/BusStop.h
@@ -0,0 +1,13 @@
+//
+//  BusStop.h
+//  GetOnThatBus
+//
+//  Created by alex fallah on 5/27/15.
+//  Copyright (c) 2015 alex fallah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BusStop : NSObject
+
+@end
---
+++ GetOnThatBus/BusStop.m
@@ -0,0 +1,13 @@
+//
+//  BusStop.m
+//  GetOnThatBus
+//
+//  Created by alex fallah on 5/27/15.
+//  Copyright (c) 2015 alex fallah. All rights reserved.
+//
+
+#import "BusStop.h"
+
+@implementation BusStop
+
+@end
